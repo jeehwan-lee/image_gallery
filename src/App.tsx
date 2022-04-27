03cf5183a7166e2528b0920c927d73735a178f28
jeehwan <tjtojan@naver.com>
2022-04-27 23:29:37 +0900

    image gallery

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import { read } from 'fs';
+import React, { useRef, useState } from 'react';
 import './App.css';
+import ImageBox from './components/imageBox';
 
 function App() {
+
+  const inpRef = useRef<HTMLInputElement>(null);
+
+  const [imageList, setImageList] = useState<string[]>([])
+  console.log(imageList);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className={'initial-box '+ (imageList.length > 0 && 'row')}>
+        {
+          imageList.length === 0 && 
+          (
+            <div className='text-center'>
+              이미지가 없습니다. <br/>
+              이미지를 추가해주세요.
+            </div>
+          )
+
+        }
+
+        {
+          imageList.map((element, index) => (
+            <ImageBox key={index} src={element}/>
+          ))
+        }
+        <input type="file" ref={inpRef} 
+        onChange={(event)=>{ 
+          if(event.currentTarget.files?.[0]) {
+
+            const file = event.currentTarget.files[0];
+
+            const reader = new FileReader();
+            reader.readAsDataURL(file);
+            reader.onloadend = (event) => {
+              setImageList(prev => [...prev, event.target?.result as string]);
+            }
+            
+            }
+          }
+        }/>
+        <div className='plus-box' onClick={()=> {
+          inpRef.current?.click()
+        }}>
+          +
+        </div>
+      </div>
     </div>
   );
 }
